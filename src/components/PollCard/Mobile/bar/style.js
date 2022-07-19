8edc1fdd1bd44c9e52d73e9c482c8fd53b2571a8
JangAyeon <jangayeon@JangAyeonui-MacBookPro.local>
2022-07-20 03:34:13 +0900

    [fix] fix typo in Vote result 투표결과 랜더링 jsx 내 오타 수정 issue : #13

--- src/components/PollCard/Mobile/bar/style.js
+++ src/components/PollCard/Mobile/bar/style.js
@@ -14,13 +14,13 @@ const Container = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  span{
-    padding : 0.4rem 0.8rem;
+  span {
+    padding: 0.4rem 0.8rem;
   }
 `;
 
 const Filler = styled.div`
-height: 100%;
+  height: 100%;
   width: ${props => `${props.voted}%`};
   background-color: ${props => props.barGround};
   border-radius: 0.5rem;
@@ -28,12 +28,10 @@ height: 100%;
   flex-direction: row;
   align-items: center;
   justify-content: flex-end;
-
 `;
 
 const Inner = styled.span`
   color: ${props => props.color};
-
 `;
 
 export { StyledRoot, Container, Filler, Inner };
