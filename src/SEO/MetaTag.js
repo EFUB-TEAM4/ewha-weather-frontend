f201d363076ec565cfd6769822c647da27f04394
JangAyeon <jangayeon@JangAyeonui-MacBookPro.local>
2022-08-03 06:18:52 +0900

    #13 check commit

--- src/SEO/MetaTag.js
+++ src/SEO/MetaTag.js
@@ -8,26 +8,26 @@ export default function MetaTag({ title, description, keywords, imgsrc, url }) {
       <title>{title}</title>
 
       <meta name="description" content={description} />
-      <meta name="keywords" content={keywords}  />
+      <meta name="keywords" content={keywords} />
 
-      <meta property="og:type" content="website"  />
-      <meta property="og:title" content={title}  />
-      <meta property="og:site_name" content={title}/>
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content={title} />
       <meta
         property="og:description"
         content={description}
         data-react-helmet="true"
       />
-      <meta property="og:image" content={imgsrc}  />
-      <meta property="og:url" content={url}  />
-      <meta name="twitter:card" content="summary"/>
-      <meta name="twitter:title" content={title}  />
+      <meta property="og:image" content={imgsrc} />
+      <meta property="og:url" content={url} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
       <meta
         name="twitter:description"
         content={description}
         data-react-helmet="true"
       />
-      <meta name="twitter:image" content={imgsrc}  />
+      <meta name="twitter:image" content={imgsrc} />
 
       <link rel="canonical" href={url} />
     </Helmet>
