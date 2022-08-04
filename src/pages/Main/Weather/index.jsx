6e4eadee7366572cfc42546a37ca36784c83186e
JangAyeon <jangayeon@JangAyeonui-MacBookPro.local>
2022-08-04 14:39:04 +0900

    #13 change Oauth

--- src/pages/Main/Weather/index.jsx
+++ src/pages/Main/Weather/index.jsx
@@ -3,9 +3,7 @@
 import React, { useEffect } from 'react';
 import { useRecoilStateLoadable } from 'recoil';
 import { BearAvater } from 'components';
-import { MainBear } from 'assets';
 import { GetCurrent } from 'state/weather';
-import MetaTag from 'SEO/MetaTag';
 import TempReport from './TempReport';
 import WeatherReport from './WeatherReport';
 import { StyledRoot, BearSection, WeatherSection } from './style';
@@ -37,17 +35,7 @@ function Weather() {
   }, [CurrentWeather, AvaterState]);
 
   return (
-    <>
-      {CurrentWeather && (
-        <MetaTag
-          title="이상청"
-          description={`지금 이화는 ${CurrentWeather.currentTemperature}도 강수확률은 ${CurrentWeather.rainfallPercentage}%입니다. 실시간 이화인들의 추천 옷차림은?`}
-          keywords="이화여대, 이화 곰돌이, 이화 날씨"
-          url="/"
-          imgsrc={MainBear}
-        />
-      )}
-      <StyledRoot>
+    <StyledRoot>
         <Title>오늘의 이화 날씨</Title>
         <ContentSection>
           {state === 'hasValue' ? (
@@ -66,7 +54,6 @@ function Weather() {
           )}
         </ContentSection>
       </StyledRoot>
-    </>
   );
 }
 
