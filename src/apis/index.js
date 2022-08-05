f968cd37aaf53fb07382a9226949863b0a18815e
JangAyeon <jangayeon@JangAyeonui-MacBookPro.local>
2022-08-06 02:49:45 +0900

    #13 fix: gitaction

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const serverAxios = axios.create({
   baseURL: `${process.env.REACT_APP_SERVER_URL}`,
-  headers: { 'Access-Control-Allow-Origin': '*' },
+  headers: { 'Access-Control-Allow-Origin':`${process.env.REACT_APP_SERVER_URL}` },
   withCredentials: false,
 });
 
