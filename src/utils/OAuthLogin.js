7738929c7e1212ffd1467fe0c9dc8364b1c636fe
JangAyeon <jangayeon@JangAyeonui-MacBookPro.local>
2022-08-06 05:14:58 +0900

    #13 test : set localstorage

--- src/utils/OAuthLogin.js
+++ src/utils/OAuthLogin.js
@@ -6,13 +6,12 @@ function createOAuthUri() {
     redirect_uri: `${process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL}`,
     /* response_type:"code", 
   flowName: 'GeneralOAuthFlow', */
-  });
+  }); 
   const AUTHORIZE_URI = `${process.env.REACT_APP_GOOGLE_OAUTH_SERVER}`;
   const OAuthRequestUri = `${AUTHORIZE_URI}?${loginQueryString}`;
-  localStorage.setItem('login url', OAuthRequestUri);
-  // const OAuthRequestUri = `${AUTHORIZE_URI}`;
-  return OAuthRequestUri;
-}
+  // localStorage.setItem('login url', OAuthRequestUri);
+ return OAuthRequestUri;
+} 
 
 // 로그인 버튼 실행시 OAuth 서버로 연결
 export default function OAuthLogin() {
